347f1e4490e140ecbb80e6ad6f1df9e16d20be44
Abdulmalik <ayodejiamzat@gmail.com>
2024-12-24 23:37:40 +0100

    j

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
   const [view, setView] = useState<string>('');
   const [range, setRange] = useState(0)
   const [edit, setEdit] = useState("")
-  const [key, setKey] = useState("")
+  const [key, setKey] = useState("");
   const [modal, setModal] = useState(false)
   const [imageModal, setImageModal] = useState(false)
   const [emailModal, setEmailModal] = useState(true)
